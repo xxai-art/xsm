de271dfae6de71558a61fef7b4ec23190d46f4ae
xxai.art <xxai.art@gmail.com>
2023-08-05 21:31:05 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub async fn xnext(
+  &self,
+  group: Bin,
+  consumer: Bin,
+  count: Option<u64>,
+  block: Option<u64>,
+  noack: bool,
+  key: Bin,
+) -> Result<Vec<(Val, Vec<(String, Vec<(Val, Val)>)>)>> {
+  let key = &Into::<Box<[u8]>>::into(key)[..];
+  let consumer = Into::<Box<[u8]>>::into(consumer);
+  let consumer = from_utf8(consumer.as_ref())?;
+  let group = Into::<Box<[u8]>>::into(group);
+  let group = from_utf8(group.as_ref())?;
+  match self
+    .c
+    .xreadgroup(group, consumer, count, block, noack, key, XID::NewInGroup)
+    .await
+  {
+    Ok(r) => Ok(r),
+    Err(err) => {
+      if err.kind() == &Unknown && err.details().starts_with("NOGROUP ") {
+        self
+          .c
+          .xgroup_create(key, group, XID::Manual("0".into()), true)
+          .await?;
+        return Ok(
+          self
+            .c
+            .xreadgroup(group, consumer, count, block, noack, key, XID::NewInGroup)
+            .await?,
+        );
+      }
+      Err(err.into())
     }
+  }
 }
