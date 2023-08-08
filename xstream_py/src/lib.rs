a8418fc44c908442199162b2e0cf8fba89c11eaa
xxai.art <xxai.art@gmail.com>
2023-08-08 11:55:29 +0800

    ♨

--- xstream_py/src/lib.rs
+++ xstream_py/src/lib.rs
@@ -1,14 +1,35 @@
+// use std::time::Duration;
+
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass]
+pub struct Server {
+  s: xstream::Server,
 }
+
+pub fn server_host_port(host: String, port: u16) -> Server {
+  Server(xstream::Server::host_port(host, port))
+}
+
+// #[pyfunction]
+// fn sleep_for(py: Python<'_>) -> PyResult<&PyAny> {
+//   pyo3_asyncio::tokio::future_into_py(py, async {
+//     tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+//     Ok(Python::with_gil(|py| py.None()))
+//   })
+// }
+//
+// /// Formats the sum of two numbers as string.
+// #[pyfunction]
+// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//   Ok((a + b).to_string())
+// }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn xstream_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    Ok(())
+  // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+  m.add_function(wrap_pyfunction!(server_host_port, m)?)?;
+  m.add_class::<Server>()?;
+  Ok(())
 }
--- xstream_py/test.py
+++ xstream_py/test.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
-from xstream_py import sum_as_string
+from xstream_py import sum_as_string, sleep_for
+import asyncio
 
-print(sum_as_string(1, 3))
+async def main():
+  print('sleep')
+  await sleep_for()
+  print(sum_as_string(1, 3))
+
+asyncio.run(main())
+
