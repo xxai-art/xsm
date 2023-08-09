2c8c6645b302e8f7c3fbec46aa9dbc128a1f04ec
xxai.art <xxai.art@gmail.com>
2023-08-09 17:02:38 +0800

    ♨

---
+++ xsm_py/test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from msgpack import unpackb
+from xsm_py import server_host_port
+import asyncio
+from os import getenv
+
+
+async def main():
+  host_port = getenv('REDIS_HOST_PORT')
+  host, port = host_port.split(':')
+  server = await server_host_port(host, int(port), 'default',
+                                  getenv('REDIS_PASSWORD'))
+
+  # server.xadd('iaa', 2, packb([]))
+  stream = server.stream("iaa")
+  limit = 32
+  while True:
+    for retry, task_id, id, args in await stream.xpendclaim(limit):
+      id = unpackb(id)
+      args = unpackb(args)
+      if retry > 6:
+        print(f"failed {retry} {task_id} {id} {args}")
+        await stream.xackdel(task_id)
+        continue
+      print(f"{retry} {task_id} {id} {args}")
+
+    print('xnext')
+    for xid, [(id, args)] in await stream.xnext(limit):
+      print(xid, unpackb(id), unpackb(args))
+
+
+asyncio.run(main())
